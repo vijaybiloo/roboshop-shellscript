166d43639826dba82e2d99d71ff8a72341ab40e6
vijaybiloo <dpk.vijay@gmail.com>
2024-04-27 10:54:08 +0530

    updated

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,9 +2,9 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0189d4870cc65fd55
-DOMAIN_NAME=joindevops.online
+IMAGE_ID=ami-0f3c7d07486cad139
+SECURITY_GROUP_ID=sg-09fa5ad0cfc669d0c
+DOMAIN_NAME=jiondevops.site
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -20,7 +20,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z0630267198Z1Q0Y8S23T --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
@@ -37,3 +37,4 @@ done
 # imporvement
 # check instance is already created or not
 # update route53 record
+# siva's AMI ID = ami-03265a0778a880afb
--- create-servers.sh
+++ create-servers.sh
@@ -2,10 +2,10 @@
 
 NAMES=$@
 INSTANCE_TYPE=""
-IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0b34d8689bd628e3f
-DOMAIN_NAME=joindevops.online
-HOSTED_ZONE_ID=Z0308214GYCUYHGJHT8R
+IMAGE_ID=ami-0f3c7d07486cad139
+SECURITY_GROUP_ID=sg-09fa5ad0cfc669d0c
+DOMAIN_NAME=jiondevops.site
+HOSTED_ZONE_ID=Z0630267198Z1Q0Y8S23T
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
